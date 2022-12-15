d77664820b59c833f756bd7b6c053df0336ecaa1
blu3 <blue@occu.cc>
2022-12-15 02:34:04 +0100

    Solved part 2

--- day_4/day_4.hs
+++ day_4/day_4.hs
@@ -7,16 +7,15 @@ import qualified Data.Set as Set
 -- Advent Of Code 2022
 -- Day 4
 
-test1 = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
-test2 = "2-4,6-8"
-test3 = "1-10,2-4"
-test4 = "12-91,13-90\n7-94,96-99\n5-85,5-81\n56-57,57-58\n26-26,27-98\n12-28,11-58\n11-32,10-32\n10-42,10-56\n46-63,38-62\n31-95,3-94\n76-82,41-75\n87-88,4-88\n21-33,20-32"
-    
-
-runTests =  do
+-- Run tests from 4.txt on part 1
+runTests1 =  do
   f <- readTestFile
   putStrLn $  testInput f
-    
+
+-- Run tests from 4.txt on part 2 
+runTests2 =  do
+  f <- readTestFile
+  putStrLn $  testInput2 f
 
 -- Read all of the test data from file
 readTestFile :: IO String
@@ -24,10 +23,6 @@ readTestFile = readFile "./4.txt"
 
 -- Count number of overlapping schedules given the string of section
 -- assignment pairs.
--- 
--- Format: Assignment := "<bound 1>-<bound 2>"
---         Schedule   := "<Assignment>, <Assignment>"
-
 testInput :: String -> String
 testInput testStr = show $ foldr (\e r -> if e then r + 1 else r) 0 pLines
   where
@@ -52,3 +47,20 @@ isOverlap line =  (s1 `Set.isSubsetOf` s2) || (s2 `Set.isSubsetOf` s1)
     l = parseLine line
     s1 = Set.fromList $ l !! 0
     s2 = Set.fromList $ l !! 1
+
+-- Test input for part 2 
+testInput2 :: String -> String
+testInput2 testStr = show $ foldr (\e r -> if e then r + 1 else r) 0 pLines
+  where
+    lines  = reverse $ drop 1 $ reverse $ splitOn "\n" testStr
+    pLines = map disjoint lines
+
+-- Return True if the pairs on line intersect at any point
+disjoint line =  not $ Set.disjoint s2 s1
+  where
+    l = parseLine line
+    s1 = Set.fromList $ l !! 0
+    s2 = Set.fromList $ l !! 1
+
+
+
